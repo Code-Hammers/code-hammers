ce7fcb0af378eadbd4ae6fa78d314a26ed17a823
Brok3Turtl3 <brok3turtl3@gmail.com>
2023-08-20 08:56:00 -0400

    FE conversion to typescript complete including scripts and webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 require("dotenv").config();
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/index.tsx",
   output: {
     path: path.resolve(__dirname, "build"),
     filename: "bundle.js",
@@ -26,34 +26,34 @@ module.exports = {
     },
   },
   module: {
-    //rules array is executed in reverse order
     rules: [
       {
-        //? matches the preceding item 0 or 1 times (could be .js or .jsx)
-        test: /\.jsx?/,
+        test: /\.(jsx|tsx|js|ts)$/,
         exclude: /(node_modules)/,
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
           },
         },
       },
 
       {
         test: /\.css$/i,
-        use: [
-          // creates 'style' nodes from JS strings
-          "style-loader",
-          // compiles CSS to commonJS
-          "css-loader",
-        ],
+        use: ["style-loader", "css-loader"],
       },
-      // handle images using file-loader - source: https://v4.webpack.js.org/loaders/file-loader/
+
       {
         test: /\.(png|jpe?g|gif)$/i,
         use: [{ loader: "file-loader" }],
       },
     ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".jsx"],
   },
 };
