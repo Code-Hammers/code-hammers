40dd9e695983e1400f3f62ae08bacf68a899ff0e
Brok3Turtl3 <brok3turtl3@gmail.com>
2023-08-20 10:00:51 -0400

    Merge conflicts resolved

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,13 +1,18 @@
+const path = require("path");
+
 module.exports = {
   preset: "ts-jest",
-  testEnvironment: "jsdom",
+  testEnvironment: "jest-environment-jsdom",
   moduleNameMapper: {
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
   },
-  setupFilesAfterEnv: ["./setupTests.ts"],
-  globals: {
-    "ts-jest": {
-      tsconfig: "tsconfig.json",
-    },
+  setupFilesAfterEnv: [path.resolve(__dirname, "./setupTests.ts")],
+  transform: {
+    "^.+\\.(ts|tsx)$": [
+      "ts-jest",
+      {
+        /* any ts-jest specific configuration here */
+      },
+    ],
   },
 };
