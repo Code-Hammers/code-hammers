b5d6c3d7a7851e31ccb6b284b524a2d702537e9e
Sean <brok3turtl3@gmail.com>
2023-09-22 21:30:26 -0400

    userRoutes refactored to return response from controllers

--- server/routes/userRoutes.ts
+++ server/routes/userRoutes.ts
@@ -9,14 +9,8 @@ import {
 const router = express.Router();
 
 router.post("/login", authUser);
-router.post("/register", registerUser, (req, res: Response) => {
-  return res.status(201).json(res.locals.user);
-});
-router.delete("/:email", deleteUserByEmail, (req, res: Response) => {
-  return res.status(200).json({ msg: "User successfully deleted!" });
-});
-router.get("/:userId", getUserById, (req, res: Response) => {
-  return res.status(200).json(res.locals.user);
-});
+router.post("/register", registerUser);
+router.delete("/:email", deleteUserByEmail);
+router.get("/:userId", getUserById);
 
 export default router;
