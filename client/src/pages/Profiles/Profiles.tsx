b2f504986d570b86443fa7647f8add18cb6f1d58
Sean <brok3turtl3@gmail.com>
2023-11-29 17:02:48 -0500

    Added profiles state, initial profiles load on profiles page

--- client/src/pages/Profiles/Profiles.tsx
+++ client/src/pages/Profiles/Profiles.tsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import { fetchProfiles } from "../../features/profiles/profilesSlice";
 
 const Profiles = (): JSX.Element => {
+  const dispatch = useAppDispatch();
+  const profiles = useAppSelector((state) => state.profiles.profiles);
+
+  useEffect(() => {
+    dispatch(fetchProfiles());
+  }, [dispatch]);
+
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-extrabold mb-4">PROFILES</h1>
-    </div>
+    <>
+      <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+        <h1 className="text-4xl font-extrabold mb-4">PROFILES</h1>
+      </div>
+      <div>
+        {profiles.map((profiles) => (
+          <div key={profiles.user}>{profiles.user}</div>
+        ))}
+      </div>
+    </>
   );
 };
 
