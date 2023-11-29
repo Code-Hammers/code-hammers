1c878c1969d6e7185bdb43a22fa35e674d7a6725
Sean <brok3turtl3@gmail.com>
2023-11-29 18:58:31 -0500

    tests refatored and updated to reflect new Profiles component structure

--- client/src/pages/Profiles/Profiles.test.tsx
+++ client/src/pages/Profiles/Profiles.test.tsx
@@ -1,17 +1,40 @@
 import React from "react";
-import { render } from "@testing-library/react";
-import "@testing-library/jest-dom";
+import { create } from "react-test-renderer";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+
 import Profiles from "./Profiles";
 
-describe("Profiles Page", () => {
-  it("renders the test H1 correctly", () => {
-    const { getByText } = render(<Profiles />);
-    const title = getByText("PROFILES");
-    expect(title).toBeInTheDocument();
-  });
+interface State {
+  profiles: {
+    profiles: { user: string }[];
+    status: 'idle' | 'loading' | 'failed';
+    error: string | null;
+  };
+}
 
-  it("matches the snapshot", () => {
-    const { asFragment } = render(<Profiles />);
-    expect(asFragment()).toMatchSnapshot();
+const mockStore = configureStore<State>([]);
+const initialState: State = {
+  profiles: {
+    profiles: [
+      { user: "User1" },
+      { user: "User2" }
+    ],
+    status: 'idle',
+    error: null
+  }
+  },
+};
+
+describe("MainPage Component", () => {
+  it("renders correctly", () => {
+    const store = mockStore(initialState);
+    const tree = create(
+      <Provider store={store}>
+        <Profiles />
+      </Provider>
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
+
