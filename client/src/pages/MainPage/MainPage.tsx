b20ec55da53c5237167464a038517fb0de0fddc5
Sean <brok3turtl3@gmail.com>
2023-12-03 15:57:07 -0500

    quick fix from a model update error

--- client/src/pages/MainPage/MainPage.tsx
+++ client/src/pages/MainPage/MainPage.tsx
@@ -7,7 +7,7 @@ const MainPage = (): JSX.Element => {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
       <h1 className="text-4xl font-extrabold mb-4">Main Page</h1>
-      <h1>Welcome {user?.name} !</h1>
+      <h1>Welcome {user?.firstName} !</h1>
     </div>
   );
 };
