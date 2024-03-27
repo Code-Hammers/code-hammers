eab47e1bda68bb4f174cf25de8b720ba12d68d4d
Brok3Turtl3 <brok3turtl3@gmail.com>
2024-03-27 09:49:17 -0400

    CHE-39 Set up routing and basic RegistrationPage component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
-import AuthenticatedApp from "./AuthenticatedApp"
+import AuthenticatedApp from "./AuthenticatedApp";
+import RegistrationPage from "./pages/RegistrationPage/RegistrationPage";
 
 const App = (): JSX.Element => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/registration/" element={<RegistrationPage />} />
         <Route path="/app/*" element={<AuthenticatedApp />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
