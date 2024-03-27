cecb9e5c894d2cc81a77d9cf343339eafe1da1a6
Brok3Turtl3 <brok3turtl3@gmail.com>
2024-03-27 08:13:00 -0400

    CHE-39 Added placeholder RegistrationPage component

---
+++ client/src/pages/RegistrationPage/RegistrationPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const RegistrationPage: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-extrabold mb-4">Main Page</h1>
+      <h1>Registration Form Goes Here!</h1>
+    </div>
+  );
+};
+
+export default RegistrationPage;
