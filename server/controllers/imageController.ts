45ecea3ec96782b5e127b545719b2b043bdeb9e1
Brok3Turtl3 <brok3turtl3@gmail.com>
2024-03-30 11:27:52 -0400

    CHE-50 Started working on imageController

---
+++ server/controllers/imageController.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+import AWS from "aws-sdk";
+
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+});
+
+const s3 = new AWS.S3();
