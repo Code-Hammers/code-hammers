d16374ad0e76d800ad74a759562f229b9c8f2738
Brok3Turtl3 <brok3turtl3@gmail.com>
2024-04-03 11:23:13 -0400

    CHE-53 Adjusted profile thumbs to display images if present and updated getAllProfiles controller to obtain presigned URLS for S3

--- client/src/components/ProfileThumb/ProfileThumb.tsx
+++ client/src/components/ProfileThumb/ProfileThumb.tsx
@@ -10,7 +10,14 @@ const ProfileThumb = ({ profile }: ProfileThumbProps): JSX.Element => {
   const dispatch = useAppDispatch();
 
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+    <div className="bg-gray-100 flex flex-col items-center justify-center p-4 rounded-lg shadow-lg">
+      {profile.profilePhoto && (
+        <img
+          src={profile.profilePhoto}
+          alt="Profile"
+          className="rounded-full h-20 w-20 object-cover mb-4"
+        />
+      )}
       <h1 className="text-4xl font-extrabold mb-4">{profile.fullName}</h1>
       <h2 className="text-4xl font-extrabold mb-4">{profile.personalBio}</h2>
     </div>
