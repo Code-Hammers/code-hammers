38f4c14e4d430220c41c76ade1aac1494965b58c
Sean <brok3turtl3@gmail.com>
2024-04-04 13:38:59 -0400

    CHE-58 Added getForumById controller and route

--- server/controllers/forumController.ts
+++ server/controllers/forumController.ts
@@ -1,4 +1,5 @@
 import Forum from "../models/forumModel";
+import Thread from "../models/threadModel";
 import { Request, Response, NextFunction } from "express";
 
 // ENDPOINT  POST api/forums
@@ -45,4 +46,35 @@ const getAllForums = async (
   }
 };
 
-export { addForum, getAllForums };
+// ENDPOINT  GET api/forums/:forumId
+// PURPOSE   Retrieve a list of all forums
+// ACCESS    all users
+const getForumById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { forumId } = req.params;
+
+  try {
+    const forum = await Forum.findById(forumId);
+    if (!forum) {
+      return res.status(404).json({ message: "Forum not found" });
+    }
+
+    const threads = await Thread.find({ forum: forumId }).populate(
+      "user",
+      "firstName lastName"
+    );
+
+    res.status(200).json({ forum, threads });
+  } catch (error) {
+    next({
+      log: `Express error in getForumById controller: ${error}`,
+      status: 500,
+      message: { err: "Server error fetching forum details" },
+    });
+  }
+};
+
+export { addForum, getAllForums, getForumById };
--- server/routes/forumRoutes.ts
+++ server/routes/forumRoutes.ts
@@ -1,10 +1,15 @@
 import express from "express";
-import { addForum, getAllForums } from "../controllers/forumController";
+import {
+  addForum,
+  getAllForums,
+  getForumById,
+} from "../controllers/forumController";
 import { protect } from "../middleware/authMiddleware"; //TODO Add admin auth middleware
 
 const router = express.Router();
 
 router.post("/", addForum);
 router.get("/", getAllForums);
+router.get("/:forumId", getForumById);
 
 export default router;
