c1fbe9179860c99c8c7fbb9175dd664e9f37ff9f
Sean <brok3turtl3@gmail.com>
2024-04-04 10:57:13 -0400

    CHE-57 Created post model

--- server/models/postModel.ts
+++ server/models/postModel.ts
@@ -1,14 +1,17 @@
 import mongoose from "mongoose";
 
-const threadSchema = new mongoose.Schema({
+const postSchema = new mongoose.Schema({
+  thread: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Thread",
+    required: true,
+  },
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  forum: { type: mongoose.Schema.Types.ObjectId, ref: "Forum", required: true },
-  title: { type: String, required: true },
   content: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
 });
 
-const Thread = mongoose.model("Thread", threadSchema);
+const Post = mongoose.model("Post", postSchema);
 
-export default Thread;
+export default Post;
