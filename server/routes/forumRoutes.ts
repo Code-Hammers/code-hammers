9567e0b50f28ff4e1c3b20f01a6f13f75c294ce6
Sean <brok3turtl3@gmail.com>
2024-04-05 12:00:09 -0400

    CHE-58 Added deletePost controller and route

--- server/routes/forumRoutes.ts
+++ server/routes/forumRoutes.ts
@@ -19,6 +19,7 @@ import {
   listPostsByThreadId,
   createPost,
   updatePost,
+  deletePost,
 } from "../controllers/postController";
 
 import { protect } from "../middleware/authMiddleware"; //TODO Add admin auth middleware
@@ -36,12 +37,13 @@ router.delete("/:forumId", protect, deleteForum); //TODO Protect with admin auth
 router.post("/:forumId/threads", protect, createThread);
 router.get("/:forumId/threads", protect, listThreadsByForumId);
 router.get("/:forumId/threads/:threadId", protect, getThreadById);
-router.put("/:forumId/threads/:threadId", protect, updateThread); //TODO Protect with admin auth
+router.put("/:forumId/threads/:threadId", protect, updateThread);
 router.delete("/:forumId/threads/:threadId", protect, deleteThread); //TODO Protect with admin auth
 
 //Post Routes
 router.get("/:forumId/threads/:threadId/posts", protect, listPostsByThreadId);
 router.post("/:forumId/threads/:threadId/posts", protect, createPost);
 router.put("/:forumId/threads/:threadId/posts/:postId", protect, updatePost);
+router.delete("/:forumId/threads/:threadId/posts/:postId", protect, deletePost); //TODO Protect with admin auth
 
 export default router;
