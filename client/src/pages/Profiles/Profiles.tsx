cedaffa08be9e2a662c39eff86915d40f8d1c900
Brok3Turtl3 <brok3turtl3@gmail.com>
2024-04-16 16:41:02 -0400

    CHE-59 Added basic styling to Profiles and ProfileThumb

--- client/src/pages/Profiles/Profiles.tsx
+++ client/src/pages/Profiles/Profiles.tsx
@@ -14,15 +14,20 @@ const Profiles = (): JSX.Element => {
 
   return (
     <>
-      <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+      <div className="min-h-screen bg-gray-900 text-white flex flex-col items-center justify-center p-4">
         <h1 className="text-4xl font-extrabold mb-4">PROFILES</h1>
-      </div>
-      <div>
-        {profiles.map((profile, index) => (
-          <Link to={`/app/profile/${profile.user}`} key={index}>
-            <ProfileThumb profile={profile} />
-          </Link>
-        ))}
+
+        <div className="w-full max-w-6xl p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {profiles.map((profile, index) => (
+            <Link
+              to={`/app/profile/${profile.user}`}
+              key={index}
+              className="shadow-lg rounded-lg overflow-hidden transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-2xl"
+            >
+              <ProfileThumb profile={profile} />
+            </Link>
+          ))}
+        </div>
       </div>
     </>
   );
