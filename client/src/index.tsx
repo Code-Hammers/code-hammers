8fa4070b1cb4c305f5290265d2425f9a17d3b4dc
seantokuzo <seantokuzo@gmail.com>
2024-05-08 17:59:05 -0700

    remove old css import

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
-// import "../dist/output.css";
 import "./input.css";
 
 import App from "./App";
