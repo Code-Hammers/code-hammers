c12ddf1296766c565ea124196b7ccb51391e2016
seantokuzo <seantokuzo@gmail.com>
2024-05-08 17:10:42 -0700

    update css import for new postcss integration

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
-import "../dist/output.css";
+// import "../dist/output.css";
+import "./input.css";
 
 import App from "./App";
 
