93724313bde81080925f6669f545d873f62fc91e
Sean <brok3turtl3@gmail.com>
2024-05-13 11:08:58 -0400

    CHE-138 Removed unnecessary code

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import "./input.css";
-//import "../dist/output.css";
 
 import App from "./App";
 
