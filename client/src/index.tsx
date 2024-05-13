7d3c87fb27eef459e69e863f25dae4c8acf8fcc5
Sean <brok3turtl3@gmail.com>
2024-05-13 10:58:41 -0400

    CHE-138 Adjusting Docker flows

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import "./input.css";
+//import "../dist/output.css";
 
 import App from "./App";
 
