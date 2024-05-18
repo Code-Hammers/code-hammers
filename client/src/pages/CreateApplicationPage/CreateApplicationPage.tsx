2d3bc44587d6e79cf595590155bacab656f32a37
Sean <brok3turtl3@gmail.com>
2024-05-17 21:03:49 -0400

    CHE-159 Added local state for formData, handleChange functionality and first input field for form

--- client/src/pages/CreateApplicationPage/CreateApplicationPage.tsx
+++ client/src/pages/CreateApplicationPage/CreateApplicationPage.tsx
@@ -4,23 +4,66 @@ import { useAppSelector } from "../../app/hooks";
 import { IApplication } from "../../../types/applications";
 
 const CreateApplicationPage = (): JSX.Element => {
-  const [application, setApplication] = useState<IApplication[]>([]);
   const user = useAppSelector((state) => state.user.userData);
 
-  useEffect(() => {
-    async function fetchApplicationById() {
-      try {
-      } catch (error) {
-        console.error("Error fetching applications:", error);
-      }
-    }
+  const [formData, setFormData] = useState({
+    title: "",
+    company: "",
+    location: "",
+    description: "",
+    url: "",
+    status_id: "",
+    quick_apply: false,
+    date_applied: new Date().toISOString().split("T")[0],
+    general_notes: "",
+  });
 
-    fetchApplicationById();
-  }, []);
+  //TODO build useEffect to get statuses
+
+  const handleChange = (
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
+    >
+  ) => {
+    const { name, value, type } = e.target;
+    if (type === "checkbox") {
+      const checked = (e.target as HTMLInputElement).checked;
+      setFormData((prevFormData) => ({
+        ...prevFormData,
+        [name]: checked,
+      }));
+    } else {
+      setFormData((prevFormData) => ({
+        ...prevFormData,
+        [name]: value,
+      }));
+    }
+  };
 
   return (
     <div className="pt-40 min-h-screen bg-gray-900 text-white flex flex-col items-center justify-center p-4">
       <h1 className="text-4xl font-extrabold mb-4">Create Applications</h1>
+      <form className="w-full max-w-lg">
+        <label className="block text-sm font-bold mb-2" htmlFor="title">
+          Job Title
+          <input
+            className="w-full p-2 rounded bg-gray-800 text-white"
+            id="title"
+            name="title"
+            type="text"
+            value={formData.title}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="submit"
+        >
+          Create Application
+        </button>
+      </form>
+      .
     </div>
   );
 };
