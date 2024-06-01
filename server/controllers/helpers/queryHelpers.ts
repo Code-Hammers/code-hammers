704bdbfc6db0ad7dcf0708b2699d50c5acb38998
Sean <brok3turtl3@gmail.com>
2024-06-01 08:10:12 -0400

    CHE-116 Typing updates

--- server/controllers/helpers/queryHelpers.ts
+++ server/controllers/helpers/queryHelpers.ts
@@ -1,14 +1,14 @@
-import { DocumentQuery } from "mongoose";
+import mongoose, { Query } from "mongoose";
+
+type SortOrder = 1 | -1;
 
 export const sortAndPopulate = (
-  query: DocumentQuery<any, any, any>,
+  query: Query<any, any, any>,
   sortField: string = "createdAt",
   sortOrder: number = -1,
   populateField: string = "user",
   selectFields: string = "firstName lastName"
 ) => {
-  return query
-    .sort({ [sortField]: sortOrder })
-    .populate(populateField, selectFields)
-    .exec();
+  const sortObj = { [sortField]: sortOrder } as { [key: string]: SortOrder };
+  return query.sort(sortObj).populate(populateField, selectFields).exec();
 };
