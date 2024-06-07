6049a2e73f457129f615ba92b735b54fd1a69133
Sean <brok3turtl3@gmail.com>
2024-06-07 09:43:09 -0400

    CHE-189 Added updateNotificationsPeriod controller

--- server/controllers/applicationsController.ts
+++ server/controllers/applicationsController.ts
@@ -1,17 +1,13 @@
-import { Request, Response, NextFunction } from "express";
-import { CustomRequest } from "../types/customRequest";
-import { pool } from "../config/sql-db";
+import { Request, Response, NextFunction } from 'express';
+import { CustomRequest } from '../types/customRequest';
+import { pool } from '../config/sql-db';
 
 interface StatusCount {
   status: string;
   count: string;
 }
 
-const getAllApplications = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+const getAllApplications = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const userId = req.query.user_id;
 
@@ -33,26 +29,22 @@ const getAllApplications = async (
     const { rows } = await pool.query(query, [userId]);
     res.json(rows);
   } catch (error) {
-    console.error("Error fetching job applications:", error);
-    res.status(500).json({ message: "Internal server error" });
+    console.error('Error fetching job applications:', error);
+    res.status(500).json({ message: 'Internal server error' });
   }
 };
 
 const getStatuses = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const { rows } = await pool.query("SELECT * FROM statuses");
+    const { rows } = await pool.query('SELECT * FROM statuses');
     res.json(rows);
   } catch (error) {
-    console.error("Error fetching statuses:", error);
-    res.status(500).json({ message: "Internal server error" });
+    console.error('Error fetching statuses:', error);
+    res.status(500).json({ message: 'Internal server error' });
   }
 };
 
-const createApplication = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+const createApplication = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const {
       title,
@@ -89,22 +81,19 @@ const createApplication = async (
       date_applied,
       general_notes,
     ];
-    const applicationResult = await pool.query(
-      applicationQuery,
-      applicationValues
-    );
+    const applicationResult = await pool.query(applicationQuery, applicationValues);
 
     res.status(201).json({ id: applicationResult.rows[0].id });
   } catch (error) {
-    console.error("Error creating application:", error);
-    res.status(500).json({ message: "Internal server error" });
+    console.error('Error creating application:', error);
+    res.status(500).json({ message: 'Internal server error' });
   }
 };
 
 const getApplicationById = async (
   req: CustomRequest<{ id: string }>,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) => {
   const { id } = req.params;
   try {
@@ -133,42 +122,31 @@ const getApplicationById = async (
     const { rows } = await pool.query(query, [id]);
 
     if (rows.length === 0) {
-      return res.status(404).json({ message: "Application not found" });
+      return res.status(404).json({ message: 'Application not found' });
     }
-    console.log("user id: ", rows[0].user_id);
+    console.log('user id: ', rows[0].user_id);
 
     if (!req.user || req.user.id !== rows[0].user_id)
-      return res
-        .status(401)
-        .json({ message: "You are not authorized to retrieve those records" });
+      return res.status(401).json({ message: 'You are not authorized to retrieve those records' });
 
     res.json(rows[0]);
   } catch (error) {
-    console.error("Error fetching application by id:", error);
-    res.status(500).json({ message: "Internal server error" });
+    console.error('Error fetching application by id:', error);
+    res.status(500).json({ message: 'Internal server error' });
   }
 };
 
 const updateApplication = async (
   req: CustomRequest<{ id: string }>,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) => {
   const { id } = req.params;
   if (!req.user || req.user.id !== id)
-    return res
-      .status(401)
-      .json({ message: "You are not authorized to retrieve those records" });
+    return res.status(401).json({ message: 'You are not authorized to retrieve those records' });
   try {
     const { id } = req.params;
-    const {
-      job_id,
-      status_id,
-      user_id,
-      quick_apply,
-      date_applied,
-      general_notes,
-    } = req.body;
+    const { job_id, status_id, user_id, quick_apply, date_applied, general_notes } = req.body;
     const query = `
       UPDATE applications
       SET job_id = $1, status_id = $2, user_id = $3, quick_apply = $4, date_applied = $5, general_notes = $6
@@ -183,23 +161,21 @@ const updateApplication = async (
       general_notes,
       id,
     ]);
-    res.status(200).json({ message: "Application updated successfully" });
+    res.status(200).json({ message: 'Application updated successfully' });
   } catch (error) {
-    console.error("Error updating application:", error);
-    res.status(500).json({ message: "Internal server error" });
+    console.error('Error updating application:', error);
+    res.status(500).json({ message: 'Internal server error' });
   }
 };
 
 const getAggregatedUserStats = async (
   req: CustomRequest<{ userId: string }>,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) => {
   const { userId } = req.params;
   if (!req.user || req.user.id !== userId)
-    return res
-      .status(401)
-      .json({ message: "You are not authorized to retrieve those records" });
+    return res.status(401).json({ message: 'You are not authorized to retrieve those records' });
   try {
     const applicationsByStatusQuery = `
       SELECT statuses.name AS status, COUNT(*) AS count
@@ -208,14 +184,13 @@ const getAggregatedUserStats = async (
       WHERE applications.user_id = $1 
       GROUP BY statuses.name
     `;
-    const applicationsByStatusResult = await pool.query<StatusCount>(
-      applicationsByStatusQuery,
-      [userId]
-    );
+    const applicationsByStatusResult = await pool.query<StatusCount>(applicationsByStatusQuery, [
+      userId,
+    ]);
 
     const totalApplications = applicationsByStatusResult.rows.reduce(
       (sum: number, row: StatusCount) => sum + parseInt(row.count, 10),
-      0
+      0,
     );
 
     res.json({
@@ -223,8 +198,26 @@ const getAggregatedUserStats = async (
       applicationsByStatus: applicationsByStatusResult.rows,
     });
   } catch (error) {
-    console.error("Error fetching aggregated data:", error);
-    res.status(500).send("Internal server error");
+    console.error('Error fetching aggregated data:', error);
+    res.status(500).send('Internal server error');
+  }
+};
+
+const updateNotificationPeriod = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { id } = req.params;
+    const { period } = req.body;
+
+    const query = `
+      UPDATE applications
+      SET notification_period = $1
+      WHERE id = $2
+    `;
+    await pool.query(query, [period, id]);
+    res.status(200).json({ message: 'Notification period updated successfully' });
+  } catch (error) {
+    console.error('Error updating notification period:', error);
+    res.status(500).json({ message: 'Internal server error' });
   }
 };
 
