09687cf40d3eff47611c9b9bcf581eca9f98af4a
Sean <brok3turtl3@gmail.com>
2024-06-07 12:56:27 -0400

    CHE-189 Added notification functionality to ApplicationPage

--- client/src/pages/ApplicationsPage/ApplicationsPage.tsx
+++ client/src/pages/ApplicationsPage/ApplicationsPage.tsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
-import { useAppSelector } from "../../app/hooks";
-import { IApplication } from "../../../types/applications";
-import ApplicationDashboard from "../../components/ApplicationDashBoard/ApplicationDashBoard";
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { useAppSelector } from '../../app/hooks';
+import { IApplication } from '../../../types/applications';
+import ApplicationDashboard from '../../components/ApplicationDashBoard/ApplicationDashBoard';
 
 const ApplicationsPage = (): JSX.Element => {
   const navigate = useNavigate();
@@ -13,24 +13,31 @@ const ApplicationsPage = (): JSX.Element => {
   useEffect(() => {
     async function fetchApplications() {
       try {
-        const response = await axios.get(
-          `/api/applications?user_id=${user?._id}`
-        );
+        const response = await axios.get(`/api/applications?user_id=${user?._id}`);
         setApplications(response.data);
       } catch (error) {
-        console.error("Error fetching applications:", error);
+        console.error('Error fetching applications:', error);
       }
     }
 
     fetchApplications();
   }, []);
 
+  const calculateIsInactive = (application: IApplication) => {
+    const { last_updated, notification_period, notifications_paused } = application;
+    if (notifications_paused) return false;
+
+    const lastUpdatedDate = new Date(last_updated);
+    const notificationPeriodMs = notification_period * 24 * 60 * 60 * 1000;
+    return new Date() - lastUpdatedDate > notificationPeriodMs;
+  };
+
   return (
     <div className="bg-gray-900 flex flex-col items-center justify-center min-h-screen p-4 pt-40 text-white">
       <ApplicationDashboard />
       <h1 className="font-extrabold mb-4 text-4xl">Applications!</h1>
       <button
-        onClick={() => navigate("/app/create-application")}
+        onClick={() => navigate('/app/create-application')}
         className="bg-blue-500 focus:shadow-outline font-bold hover:bg-blue-700 mb-4 py-2 px-4 rounded selection:focus:outline-none text-white"
       >
         Create New Application
@@ -42,17 +49,16 @@ const ApplicationsPage = (): JSX.Element => {
               <div className="font-bold text-lg ">
                 {application.title} at {application.company}
               </div>
-              <div className="text-gray-400 text-sm ">
-                Status: {application.status}
-              </div>
-              <div className="text-gray-400 text-sm ">
-                Notes: {application.general_notes}
-              </div>
+              <div className="text-gray-400 text-sm ">Status: {application.status}</div>
+              <div className="text-gray-400 text-sm ">Notes: {application.general_notes}</div>
+              {calculateIsInactive(application) && (
+                <div className="text-red-500 text-sm font-bold">
+                  This application needs attention!
+                </div>
+              )}
               <button
                 className="bg-blue-500 focus:outline-none focus:shadow-outline font-bold hover:bg-blue-700 mt-2 px-4 py-2 rounded text-white"
-                onClick={() =>
-                  navigate(`/app/update-application/${application.id}`)
-                }
+                onClick={() => navigate(`/app/update-application/${application.id}`)}
               >
                 Update Application
               </button>
