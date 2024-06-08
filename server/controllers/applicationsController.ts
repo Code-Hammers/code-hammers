192adadbbb1ed3530efea8bed0d23621e075020d
Brok3Turtl3 <brok3turtl3@gmail.com>
2024-06-08 11:57:17 -0400

    shakes fist at linter

--- server/controllers/applicationsController.ts
+++ server/controllers/applicationsController.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response } from 'express';
 import { CustomRequest } from '../types/customRequest';
 import { pool } from '../config/sql-db';
 
