8e222aa420d9d689aca838882b899b9fe8bb2704
seantokuzo <seantokuzo@gmail.com>
2024-06-08 23:44:57 -0700

    update old error condition in global error handler

--- server/middleware/errorHandler.ts
+++ server/middleware/errorHandler.ts
@@ -4,7 +4,7 @@ import { CustomError, InternalError } from '../errors';
 interface OldError {
   status: number;
   log: string;
-  message: string;
+  message: { err: string };
 }
 
 const errorHandler = (err: Error | OldError, _req: Request, res: Response, _next: NextFunction) => {
@@ -17,7 +17,7 @@ const errorHandler = (err: Error | OldError, _req: Request, res: Response, _next
   // Handles errors thrown the old way
   if (!(err instanceof Error) && err.status && err.log && err.message) {
     console.log(err.log);
-    return res.status(err.status).send([{ message: err.message }]);
+    return res.status(err.status).send([{ message: err.message.err }]);
   }
 
   // if it's an unhandled/unknown error, send back a generic error
