eff0e420bcb90f57c44507824f9983e9a64fa15e
seantokuzo <seantokuzo@gmail.com>
2024-06-09 00:02:06 -0700

    comment grammar cleanup

--- server/middleware/errorHandler.ts
+++ server/middleware/errorHandler.ts
@@ -8,19 +8,19 @@ interface OldError {
 }
 
 const errorHandler = (err: Error | OldError, _req: Request, res: Response, _next: NextFunction) => {
-  // if it's one of our custom errors, use it's properties/method
+  // If it is one of our custom errors use its properties/method
   if (err instanceof CustomError) {
     console.log(err.message);
     return res.status(err.statusCode).send(err.serializeErrors());
   }
 
-  // Handles errors thrown the old way
+  // Handle errors thrown the old way
   if (!(err instanceof Error) && err.status && err.log && err.message) {
     console.log(err.log);
     return res.status(err.status).send([{ message: err.message.err }]);
   }
 
-  // if it's an unhandled/unknown error, send back a generic error
+  // If it is an unknown error send back a generic error
   const internalError = new InternalError();
   return res.status(internalError.statusCode).send(internalError.serializeErrors());
 };
