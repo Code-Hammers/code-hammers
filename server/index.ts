16d2fbcc7b8ea68dd9e66491d8d80c13a72a9ad1
seantokuzo <seantokuzo@gmail.com>
2024-06-10 19:28:13 -0700

    linty mcLinterson

--- server/index.ts
+++ server/index.ts
@@ -57,7 +57,7 @@ const PORT: number = Number(process.env.PORT) || 3000;
 
 export const startServer = () => {
   connectDB();
-  
+
   return app.listen(PORT, () =>
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`),
   );
