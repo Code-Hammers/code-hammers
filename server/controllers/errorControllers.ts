22551a3994983c14cfdd4e9ee708a529741832a5
seantokuzo <seantokuzo@gmail.com>
2024-06-10 18:55:53 -0700

    remove unused catch all controller

--- server/controllers/errorControllers.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-
-const notFound = (req: Request, res: Response, next: NextFunction): void => {
-  const error = new Error(`Not found - ${req.originalUrl}`);
-  res.status(404);
-  next(error);
-};
-
-export { notFound };
