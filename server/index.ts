4053e888683f802feb069c3041655062b6fba1ad
seantokuzo <seantokuzo@gmail.com>
2024-06-12 13:02:00 -0700

    cleanup env var check error messages

--- server/index.ts
+++ server/index.ts
@@ -10,14 +10,14 @@ export const startServer = async () => {
   // Environment variable checks
   if (!process.env.JWT_SECRET) throw Error('❌ JWT_SECRET must be defined!');
   if (!process.env.MONGO_URI) throw Error('❌ MONGO_URI must be defined!');
-  if (!process.env.POSTGRES_USER) throw Error('❌ # POSTGRES_USER must be defined!');
-  if (!process.env.POSTGRES_DB) throw Error('❌ # POSTGRES_DB must be defined!');
-  if (!process.env.POSTGRES_PASSWORD) throw Error('❌ # POSTGRES_PASSWORD must be defined!');
-  if (!process.env.AWS_ACCESS_KEY_ID) throw Error('❌ # AWS_ACCESS_KEY_ID must be defined!');
+  if (!process.env.POSTGRES_USER) throw Error('❌ POSTGRES_USER must be defined!');
+  if (!process.env.POSTGRES_DB) throw Error('❌ POSTGRES_DB must be defined!');
+  if (!process.env.POSTGRES_PASSWORD) throw Error('❌ POSTGRES_PASSWORD must be defined!');
+  if (!process.env.AWS_ACCESS_KEY_ID) throw Error('❌ AWS_ACCESS_KEY_ID must be defined!');
   if (!process.env.AWS_SECRET_ACCESS_KEY)
-    throw Error('❌ # AWS_SECRET_ACCESS_KEY must be defined!');
-  if (!process.env.AWS_REGION) throw Error('❌ # AWS_REGION must be defined!');
-  if (!process.env.BUCKET_NAME) throw Error('❌ # BUCKET_NAME must be defined!');
+    throw Error('❌ AWS_SECRET_ACCESS_KEY must be defined!');
+  if (!process.env.AWS_REGION) throw Error('❌ AWS_REGION must be defined!');
+  if (!process.env.BUCKET_NAME) throw Error('❌ BUCKET_NAME must be defined!');
 
   // Connect to MongoDB
   await connectDB(process.env.MONGO_URI);
