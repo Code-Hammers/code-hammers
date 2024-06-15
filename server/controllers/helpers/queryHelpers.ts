768ba565f788bf7e087bc0f8498c666cd2c9188f
Howard Sun <howardsun@pm.me>
2024-06-15 17:47:21 -0400

    CHE-120 added changes from CHE-129 to avoid merge conflict

--- server/controllers/helpers/queryHelpers.ts
+++ server/controllers/helpers/queryHelpers.ts
@@ -2,13 +2,18 @@ import mongoose, { Query, Aggregate } from "mongoose";
 
 type SortOrder = 1 | -1;
 
-// ? Should the sortOrder here be number or SortOrder type?
-export const sortAndPopulate = (
-  query: Query<any, any, any>,
-  sortField: string = "createdAt",
+interface SortAndPopulateQuery<T> {
+  sort: (arg: { [key: string]: SortOrder }) => SortAndPopulateQuery<T>;
+  populate: (field: string, select?: string) => SortAndPopulateQuery<T>;
+  exec: () => Promise<T>;
+}
+
+export const sortAndPopulate = <T>(
+  query: SortAndPopulateQuery<T>,
+  sortField: string = 'createdAt',
   sortOrder: number = -1,
-  populateField: string = "user",
-  selectFields: string = "firstName lastName"
+  populateField: string = 'user',
+  selectFields: string = 'firstName lastName',
 ) => {
   const sortObj = { [sortField]: sortOrder } as { [key: string]: SortOrder };
   return query.sort(sortObj).populate(populateField, selectFields).exec();
