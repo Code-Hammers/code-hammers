75422f7e6dacdd92a1e27c7414be63f7d7d52fbd
Mnelson98 <138735614+Mnelson98@users.noreply.github.com>
2024-06-17 16:05:06 -0400

    Added company and title to profile cards and ensuring bio doesnt spill over

--- client/src/components/ProfileThumb/ProfileThumb.tsx
+++ client/src/components/ProfileThumb/ProfileThumb.tsx
@@ -7,9 +7,17 @@ interface ProfileThumbProps {
 const ProfileThumb = ({ profile }: ProfileThumbProps) => {
   const defaultImage = 'https://picsum.photos/200';
 
+  const shortenBio = (bio: string | undefined, wordLimit: number) => {
+    if (!bio) return;
+    const words: string[] = bio.split(' ');
+    if (words.length > wordLimit) {
+      return words.slice(0, wordLimit).join(' ') + '...';
+    }
+    return bio;
+  };
+
   return (
-    <div className="bg-gradient-to-r flex flex-col from-gray-700 h-64 items-center justify-center p-4 rounded-lg text-white to-gray-900 via-gray-800 w-64
-    ">
+    <div className="bg-gradient-to-r flex flex-col from-gray-700 h-64 items-center justify-center p-4 rounded-lg text-white to-gray-900 via-gray-800 w-64">
       <img
         src={profile.profilePhoto || defaultImage}
         alt={profile.firstName || 'Default Profile'}
@@ -19,7 +27,10 @@ const ProfileThumb = ({ profile }: ProfileThumbProps) => {
       <h1 className="font-bold mb-2 text-xl">
         {profile.firstName} {profile.lastName}
       </h1>
-      <h2 className="mb-2 text-md ">{profile.personalBio}</h2>
+      <h1 className="font-bold mb-2 text-xs">
+        {profile.careerInformation?.currentPosition?.title} @ {profile.careerInformation?.currentPosition?.company}
+      </h1>
+      <h2 className="max-h-10 mb-2 text-xs">{shortenBio(profile.personalBio, 8)}</h2>
     </div>
   );
 };
