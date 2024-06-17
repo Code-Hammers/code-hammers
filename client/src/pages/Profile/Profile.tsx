7f98b218c64367b9ee8a33109f4d12b40329a124
Mnelson98 <138735614+Mnelson98@users.noreply.github.com>
2024-06-17 16:26:55 -0400

    updated styling

--- client/src/pages/Profile/Profile.tsx
+++ client/src/pages/Profile/Profile.tsx
@@ -12,19 +12,19 @@ const Profile = () => {
     if (userId) dispatch(fetchUserProfile(userId));
   }, [dispatch]);
   return (
-    <div className="min-h-screen bg-gray-900 text-white flex flex-col items-center justify-start pt-20 p-4">
-      <h1 className="text-4xl font-extrabold mb-4 mt-16">Profile</h1>
-      <div className="w-full max-w-4xl bg-gradient-to-r from-gray-700 via-gray-800 to-gray-900 pt-6 pb-6 pl-6 pr-6 rounded-lg shadow-lg flex flex-col items-center">
+    <div className="bg-gray-900 flex flex-col items-center justify-start min-h-screen p-4 pt-20 text-white">
+      <h1 className="font-extrabold mb-4 mt-16 text-4xl">Profile</h1>
+      <div className="bg-gradient-to-r flex flex-col from-gray-700 items-center max-w-4xl pb-6 pl-6 pr-6 pt-6 rounded-lg shadow-lg to-gray-900 via-gray-800 w-full">
         <img
           src={userProfile?.profilePhoto || 'https://picsum.photos/200'}
           alt="Profile"
-          className="rounded-full h-32 w-32 object-cover mb-4 mt-4"
+          className="h-32 mb-4 mt-4 object-cover rounded-full w-32"
         />
-        <h2 className="text-xl font-bold mb-2">
+        <h2 className="font-bold mb-2 text-xl">
           {userProfile?.firstName} {userProfile?.lastName}
         </h2>
         <h3>{userProfile?.cohort}</h3>
-        <p className="text-lg mb-4">{userProfile?.personalBio}</p>
+        <p className="mb-4 text-lg">{userProfile?.personalBio}</p>
       </div>
     </div>
   );
