684465302096714c4e41821745243fb1eedd60fe
seantokuzo <seantokuzo@gmail.com>
2024-06-19 10:05:32 -0700

    update config with setup files

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,26 @@
+// module.exports = {
+//   roots: ['<rootDir>/server'],
+//   transform: {
+//     '^.+\\.tsx?$': 'ts-jest',
+//   },
+//   setupFilesAfterEnv: ['<rootDir>/server/test/setup.ts'],
+//   // testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+//   testRegex: '.test.ts?x$',
+//   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+
+// };
+
 module.exports = {
-  roots: ['<rootDir>/__tests__'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  setupFilesAfterEnv: ['<rootDir>/server/test/setup.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules',
+    '<rootDir>/dist',
+    '<rootDir>/coverage',
+    '<rootDir>/client/node_modules',
+    '<rootDir>/client/build',
+    '<rootDir>/client/coverage',
+  ],
 };
