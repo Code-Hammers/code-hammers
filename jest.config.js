66fac5294e65c2a756a9ea4dcfad4ff919570e41
seantokuzo <seantokuzo@gmail.com>
2024-06-20 11:36:07 -0700

    adjust config to only handle server tests

--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,13 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'jest-environment-jsdom',
-  setupFilesAfterEnv: ['<rootDir>/client/setupTests.ts'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
-  moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/client/__mocks__/fileMock.js',
-  },
-  coveragePathIgnorePatterns: [],
+  testEnvironment: 'node',
+  setupFilesAfterEnv: ['<rootDir>/server/test/setup.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
   testPathIgnorePatterns: [
     '<rootDir>/scripts',
     '<rootDir>/node_modules',
     '<rootDir>/dist',
     '<rootDir>/coverage',
-    '<rootDir>/client/node_modules',
-    '<rootDir>/client/build',
-    '<rootDir>/client/coverage',
-    '<rootDir>/client/src/assets',
+    '<rootDir>/client',
   ],
 };
