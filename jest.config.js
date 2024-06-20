981c9f11f0f64c80fcf53a79eca6f66a532bc538
seantokuzo <seantokuzo@gmail.com>
2024-06-20 09:47:20 -0700

    update jest config for server tests - needs more work

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,25 @@
 module.exports = {
-  roots: ['<rootDir>/__tests__'],
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/client/setupTests.ts'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/client/__mocks__/fileMock.js',
+  },
+  coveragePathIgnorePatterns: [],
+  testPathIgnorePatterns: [
+    '<rootDir>/scripts',
+    '<rootDir>/node_modules',
+    '<rootDir>/dist',
+    '<rootDir>/coverage',
+    '<rootDir>/client/node_modules',
+    '<rootDir>/client/build',
+    '<rootDir>/client/coverage',
+    '<rootDir>/client/src/assets',
+  ],
 };
