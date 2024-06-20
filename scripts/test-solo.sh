513fff2df7420c4c73570c315a349a388ac38615
seantokuzo <seantokuzo@gmail.com>
2024-06-20 11:57:57 -0700

    add prompt loop

--- scripts/test-solo.sh
+++ scripts/test-solo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-RED='\033[0;31m'
+RED='\033[1;31m'
 YELLOW='\033[1;33m'
-GREEN='\033[32;1m'
+GREEN='\033[1;32;1m'
 CORNBLUE="\033[1;3;38;5;69m"
 NC='\033[0m' # No color
 
@@ -18,12 +18,18 @@ fi
 printf $"${YELLOW}\nAre you running a server or client test? (Enter S for server or C for client) [S/C]${NC}\n"
 read -r SC
 
+while [[ $SC != 'S' && $SC != 's' && $SC != '' && $SC != 'C' && $SC != 'c' ]]; do
+  echo -e "${RED}\nCommand not recognized. Please choose ${GREEN}S${RED} for server test or ${GREEN}C${RED} for client test (or Ctrl C to cancel)${NC}\n"
+  printf $"${YELLOW}\nAre you running a server or client test? (Enter S for server or C for client) [S/C]${NC}\n"
+  read -r SC
+done
+
 if [[ $SC == "S" || $SC == "s" || $SC == "" ]]; then
-  echo -e "${GREEN}Gotcha. Looking for test files matching \"$1\" in the server${NC}\n"
+  echo -e "${GREEN}\nGotcha. Looking for test files matching \"$1\" in the server${NC}\n"
   TEST_CMD="test" TEST_FILE=$1 docker-compose -f docker-compose-test-solo.yml up --abort-on-container-exit
 fi
 
 if [[ $SC == "C" || $SC == "c" ]]; then
-  echo -e "${GREEN}Gotcha. Looking for test files matching \"$1\" in the client${NC}\n"
+  echo -e "${GREEN}\nGotcha. Looking for test files matching \"$1\" in the client${NC}\n"
   TEST_CMD="test:client" TEST_FILE=$1 docker-compose -f docker-compose-test-solo.yml up --abort-on-container-exit
 fi
