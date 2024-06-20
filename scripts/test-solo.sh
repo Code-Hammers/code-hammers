35ad808a711f71400299ba1b3fe7c301e49ec137
seantokuzo <seantokuzo@gmail.com>
2024-06-20 11:35:33 -0700

    add 'server or client' prompt to solo test script

--- scripts/test-solo.sh
+++ scripts/test-solo.sh
@@ -2,6 +2,7 @@
 
 RED='\033[0;31m'
 YELLOW='\033[1;33m'
+GREEN='\033[32;1m'
 CORNBLUE="\033[1;3;38;5;69m"
 NC='\033[0m' # No color
 
@@ -14,5 +15,15 @@ if [ -z $1 ]; then
   exit
 fi
 
-echo -e "${CORNBLUE}Running tests matching the file name \"$1\"...${NC}\n"
-TEST_FILE=$1 docker-compose -f docker-compose-test-solo.yml up --abort-on-container-exit
+printf $"${YELLOW}\nAre you running a server or client test? (Enter S for server or C for client) [S/C]${NC}\n"
+read -r SC
+
+if [[ $SC == "S" || $SC == "s" || $SC == "" ]]; then
+  echo -e "${GREEN}Gotcha. Looking for test files matching \"$1\" in the server${NC}\n"
+  TEST_CMD="test" TEST_FILE=$1 docker-compose -f docker-compose-test-solo.yml up --abort-on-container-exit
+fi
+
+if [[ $SC == "C" || $SC == "c" ]]; then
+  echo -e "${GREEN}Gotcha. Looking for test files matching \"$1\" in the client${NC}\n"
+  TEST_CMD="test:client" TEST_FILE=$1 docker-compose -f docker-compose-test-solo.yml up --abort-on-container-exit
+fi
