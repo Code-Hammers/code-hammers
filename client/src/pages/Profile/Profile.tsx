389344e258b7bcc3810f9c455e60b2cddf69e2b8
Mnelson98 <138735614+Mnelson98@users.noreply.github.com>
2024-06-20 16:43:57 -0400

    destructured user profile and added networking status

--- client/src/pages/Profile/Profile.tsx
+++ client/src/pages/Profile/Profile.tsx
@@ -10,25 +10,44 @@ const Profile = () => {
 
   useEffect(() => {
     if (userId) dispatch(fetchUserProfile(userId));
-  }, [dispatch]);
+  }, [dispatch, userId]);
+
+
+  const {
+    profilePhoto, 
+    firstName,
+    lastName, 
+    cohort, 
+    careerInformation,
+    availabilityForNetworking,
+    personalBio,
+  } = userProfile || {};
+
+  const position = careerInformation?.currentPosition;
+  const networkingStatus = availabilityForNetworking 
+    ? `I'm available for networking!`
+    : `I'm not available for networking right now`;
+
   return (
     <div className="bg-gray-900 flex flex-col items-center justify-start min-h-screen p-4 pt-20 text-white">
       <h1 className="font-extrabold mb-4 mt-16 text-4xl">Profile</h1>
       <div className="bg-gradient-to-r flex flex-col from-gray-700 items-center max-w-4xl pb-6 pl-6 pr-6 pt-6 rounded-lg shadow-lg to-gray-900 via-gray-800 w-full">
         <img
-          src={userProfile?.profilePhoto || 'https://picsum.photos/200'}
+          src={profilePhoto || 'https://picsum.photos/200'}
           alt="Profile"
           className="h-32 mb-4 mt-4 object-cover rounded-full w-32"
         />
         <h2 className="font-bold mb-1 text-xl">
-          {userProfile?.firstName} {userProfile?.lastName}
+          {firstName} {lastName}
         </h2>
-        <h3 className='text-m'>{userProfile?.cohort}</h3>
+        <h3 className='text-m'>{cohort}</h3>
         <h2 className="font-bold mb-2 text-l">
-          {userProfile?.careerInformation?.currentPosition?.title} @ {userProfile?.careerInformation?.currentPosition?.company}
+          {position?.title} @ {position?.company}
         </h2>
-        
-        <p className="mb-4 text-lg">{userProfile?.personalBio}</p>
+        <h3 className="font-bold mb-2 text-xs">
+          {networkingStatus}
+        </h3>
+        <p className="mb-4 text-s">{personalBio}</p>
       </div>
     </div>
   );
