7bf46665199da417672cf3a339893d571e18d7d5
Sean <brok3turtl3@gmail.com>
2024-06-21 10:37:40 -0400

    CHE-84 uathUser refactored for new error handling

--- server/controllers/userController/authUser/authUser.ts
+++ server/controllers/userController/authUser/authUser.ts
@@ -1,6 +1,7 @@
 import User from '../../../models/userModel';
 import generateToken from '../../../utils/generateToken';
 import { Request, Response, NextFunction } from 'express';
+import { NotAuthorizedError, ValidationError, RequestValidationError } from '../../../errors';
 import { UserType } from '../../../types/user';
 
 // ENDPOINT  POST api/users/login
@@ -11,46 +12,42 @@ const authUser = async (req: Request, res: Response, next: NextFunction) => {
 
   const isValidEmail = email.match(/[\w\d.]+@[a-z]+.[\w]+$/gim);
   if (!isValidEmail) {
-    return res.status(400).json({ msg: 'Please enter a valid email' }); //TODO Move to global error handler
+    throw new RequestValidationError([new ValidationError('Please enter a valid email', 'email')]);
   }
 
   if (!email || !password) {
-    return res.status(400).json({ msg: 'Email and password are required!' }); //TODO Move to global error handler
+    if (!email)
+      throw new RequestValidationError([new ValidationError('You must enter an email', 'email')]);
+    if (!password)
+      throw new RequestValidationError([
+        new ValidationError('You must enter a password', 'password'),
+      ]);
   }
 
-  try {
-    const user: UserType | null = await User.findOne({ email });
+  const user: UserType | null = await User.findOne({ email });
 
-    if (!user) {
-      return res.status(401).json({ msg: 'User not found!' }); //TODO Move to global error handler
-    }
+  if (!user) {
+    throw new NotAuthorizedError();
+  }
 
-    if (user && (await user.matchPassword!(password))) {
-      res.locals.user = {
-        _id: user._id,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        email: user.email,
-      };
-      const token = generateToken(user._id.toString());
+  if (user && (await user.matchPassword!(password))) {
+    res.locals.user = {
+      _id: user._id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+    };
+    const token = generateToken(user._id.toString());
 
-      res.cookie('token', token, {
-        httpOnly: true,
-        expires: new Date(Date.now() + 3600000),
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: 'strict',
-      });
-      return res.status(200).json(res.locals.user);
-    } else {
-      return res.status(401).json({ msg: 'Incorrect password' }); //TODO Move to global error handler
-    }
-  } catch (error) {
-    console.error('Error during user authentication:', error);
-    return next({
-      log: 'Express error in createUser Middleware',
-      status: 503,
-      message: { err: 'An error occurred during login' },
+    res.cookie('token', token, {
+      httpOnly: true,
+      expires: new Date(Date.now() + 3600000),
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
     });
+    return res.status(200).json(res.locals.user);
+  } else {
+    throw new NotAuthorizedError();
   }
 };
 
