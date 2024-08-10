917acb64fa231242762daee3046e146ce18afe64
Sean <brok3turtl3@gmail.com>
2024-08-10 11:16:27 -0400

    CHE-202 Refactored deleteUserByEmail to use custom error handlers

--- server/controllers/userController/deleteUserByEmail/deleteUserByEmail.ts
+++ server/controllers/userController/deleteUserByEmail/deleteUserByEmail.ts
@@ -1,5 +1,6 @@
 import User from '../../../models/userModel';
 import { Request, Response, NextFunction } from 'express';
+import { NotFoundError } from '../../../errors';
 import { UserType } from '../../../types/user';
 
 // ENDPOINT  DELETE api/users/:email
@@ -8,21 +9,13 @@ import { UserType } from '../../../types/user';
 const deleteUserByEmail = async (req: Request, res: Response, next: NextFunction) => {
   const { email } = req.params;
 
-  try {
-    const user: UserType | null = await User.findOneAndDelete({ email });
+  const user: UserType | null = await User.findOneAndDelete({ email });
 
-    if (!user) {
-      return res.status(404).json({ msg: 'User not found!' }); //TODO Move to global error handler
-    }
-
-    return res.status(200).json({ msg: 'User successfully deleted!' });
-  } catch (error) {
-    return next({
-      log: 'Express error in getUserByEmail Middleware',
-      status: 500,
-      message: { err: 'An error occurred during removal' },
-    });
+  if (!user) {
+    throw new NotFoundError();
   }
+
+  return res.status(200).json({ msg: 'User successfully deleted!' });
 };
 
 export default deleteUserByEmail;
