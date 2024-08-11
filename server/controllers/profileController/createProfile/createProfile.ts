1aaf5d1122d6a5dcb9b404b67fcf6f6298a44a4a
Sean <brok3turtl3@gmail.com>
2024-08-11 09:07:00 -0400

    CHE-203 Removed try/catch and Next import

--- server/controllers/profileController/createProfile/createProfile.ts
+++ server/controllers/profileController/createProfile/createProfile.ts
@@ -33,47 +33,36 @@ const createProfile = async (req: Request, res: Response, next: NextFunction) =>
     blogOrWriting,
   } = req.body;
 
-  try {
-    const profile = await Profile.create({
-      user,
-      fullName,
-      nickname,
-      profilePhoto,
-      cohort,
-      graduationYear,
-      email,
-      linkedInProfile,
-      gitHubProfile,
-      professionalSummary,
-      skills,
-      specializations,
-      careerInformation,
-      education,
-      projects,
-      personalBio,
-      testimonials,
-      socialMediaLinks,
-      availabilityForNetworking,
-      bootcampExperience,
-      achievementsAndCertifications,
-      volunteerWork,
-      eventParticipation,
-      gallery,
-      blogOrWriting,
-    });
+  const profile = await Profile.create({
+    user,
+    fullName,
+    nickname,
+    profilePhoto,
+    cohort,
+    graduationYear,
+    email,
+    linkedInProfile,
+    gitHubProfile,
+    professionalSummary,
+    skills,
+    specializations,
+    careerInformation,
+    education,
+    projects,
+    personalBio,
+    testimonials,
+    socialMediaLinks,
+    availabilityForNetworking,
+    bootcampExperience,
+    achievementsAndCertifications,
+    volunteerWork,
+    eventParticipation,
+    gallery,
+    blogOrWriting,
+  });
 
-    if (profile) {
-      return res.status(201).json(profile);
-    }
-  } catch (error) {
-    console.error(error);
-    return next({
-      log: 'Express error in createProfile Middleware',
-      status: 500,
-      message: {
-        err: 'An error occurred during profile creation. Please try again.',
-      },
-    });
+  if (profile) {
+    return res.status(201).json(profile);
   }
 };
 
