d008c4de48fe96efd9fe7d70d1788c75eedbadc9
Brok3Turtl3 <brok3turtl3@gmail.com>
2024-10-17 16:34:57 -0400

    Added books.component logic and boilerplate books template

--- angular-resources-page/src/app/pages/books/books.component.ts
+++ angular-resources-page/src/app/pages/books/books.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface Book {
+  id: string;
+  title: string;
+  author: string;
+  link: string;
+}
 
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
-  styleUrls: ['./books.component.css']
+  styleUrls: ['./books.component.scss'],
 })
 export class BooksComponent implements OnInit {
+  books: Book[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.http
+      .get<Book[]>('http://localhost:8080/api/books')
+      .subscribe((data) => (this.books = data));
   }
-
 }
