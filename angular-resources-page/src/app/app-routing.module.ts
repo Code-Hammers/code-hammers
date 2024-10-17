8730e5e45f531bd28f448ed4ad3921fc844e06b1
Brok3Turtl3 <brok3turtl3@gmail.com>
2024-10-17 11:17:23 -0400

    Basic routing module complete

--- angular-resources-page/src/app/app-routing.module.ts
+++ angular-resources-page/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PeopleComponent } from './pages/people/people.component';
+import { WebsitesComponent } from './pages/websites/websites.component';
+import { BooksComponent } from './pages/books/books.component';
+import { ToolsComponent } from './pages/tools/tools.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent }, // Default route (Home)
+  { path: 'people', component: PeopleComponent },
+  { path: 'websites', component: WebsitesComponent },
+  { path: 'books', component: BooksComponent },
+  { path: 'tools', component: ToolsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
